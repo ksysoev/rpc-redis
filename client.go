aa4e62f2a068a3f50250a7c44a48b52f178f1898
Kirill Sysoev <k.sysoev@me.com>
2024-07-06 20:40:45 +0800

    Implements logic for sending request

--- client.go
+++ client.go
@@ -4,30 +4,44 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"sync"
 
 	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
 
+type Response string
+
+type rawResponse struct {
+	ID     string `json:"id"`
+	Result string `json:"result"`
+	Error  string `json:"error"`
+}
+
 type Client struct {
-	redis   *redis.Client
-	id      string
-	ctx     context.Context
-	cancel  context.CancelFunc
-	wg      *sync.WaitGroup
-	channel string
+	redis    *redis.Client
+	id       string
+	ctx      context.Context
+	cancel   context.CancelFunc
+	wg       *sync.WaitGroup
+	channel  string
+	requests map[string]chan<- Response
+	lock     *sync.RWMutex
 }
 
 func NewClient(redis *redis.Client, channel string) *Client {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	client := &Client{
-		redis:   redis,
-		id:      uuid.New().String(),
-		ctx:     ctx,
-		cancel:  cancel,
-		channel: channel,
+		redis:    redis,
+		id:       uuid.New().String(),
+		ctx:      ctx,
+		cancel:   cancel,
+		channel:  channel,
+		requests: make(map[string]chan<- Response),
+		wg:       &sync.WaitGroup{},
+		lock:     &sync.RWMutex{},
 	}
 
 	go client.handleResponses()
@@ -45,6 +59,9 @@ func (c *Client) Call(ctx context.Context, method string, params any) (string, e
 		return "", fmt.Errorf("error marshalling params: %w", err)
 	}
 
+	respChan := c.addRequest(c.id)
+	defer c.removeRequest(c.id)
+
 	msg := map[string]interface{}{
 		"method":   method,
 		"params":   paramsBytes,
@@ -60,7 +77,12 @@ func (c *Client) Call(ctx context.Context, method string, params any) (string, e
 		return "", fmt.Errorf("error sending request: %w", err)
 	}
 
-	return "", nil
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case resp := <-respChan:
+		return string(resp), nil
+	}
 }
 
 func (c *Client) Close() {
@@ -78,7 +100,41 @@ func (c *Client) handleResponses() {
 		case <-c.ctx.Done():
 			return
 		case msg := <-pubsubChan:
-			fmt.Println(msg.Payload)
+			var rawResp rawResponse
+
+			err := json.Unmarshal([]byte(msg.Payload), &rawResp)
+			if err != nil {
+				slog.Error("Error unmarshalling response: " + err.Error())
+				continue
+			}
+
+			c.lock.RLock()
+			respChan, ok := c.requests[rawResp.ID]
+			c.lock.RUnlock()
+
+			if !ok {
+				continue
+			}
+
+			respChan <- Response(rawResp.Result)
 		}
 	}
+}
+
+func (c *Client) addRequest(id string) <-chan Response {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	respChan := make(chan Response)
+
+	c.requests[id] = respChan
+
+	return respChan
+}
+
+func (c *Client) removeRequest(id string) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	delete(c.requests, id)
 }
