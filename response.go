e42e6e18c2003991a5d9f1529f98e90ff25d2390
Kirill Sysoev <k.sysoev@me.com>
2024-07-09 12:00:39 +0800

    Adds documentation for response structure

--- response.go
+++ response.go
@@ -2,12 +2,15 @@ package redisrpc
 
 import "encoding/json"
 
+// Response represents the response structure for a Redis RPC call.
 type Response struct {
-	ID     string          `json:"id"`
-	Result json.RawMessage `json:"result,omitempty"`
-	Error  string          `json:"error,omitempty"`
+	ID     string          `json:"id"`               // ID is the unique identifier for the request.
+	Result json.RawMessage `json:"result,omitempty"` // Result contains the response result, if any.
+	Error  string          `json:"error,omitempty"`  // Error contains the error message, if any.
 }
 
+// ParseResut parses the result of the response into the provided value.
+// The result is expected to be in JSON format and will be unmarshaled into the provided value.
 func (r *Response) ParseResut(v interface{}) error {
 	return json.Unmarshal(r.Result, v)
 }
